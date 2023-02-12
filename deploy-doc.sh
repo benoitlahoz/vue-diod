67f4b5f0ac3abe561b3cc85a4e1c3d4958218444
Benoît LAHOZ <lahoz.ben@gmail.com>
2023-02-12 22:51:35 +0100

    Documentation.

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
+
+yarn docs:build
+
+git subtree push --prefix docs/.vitepress/dist main gh-pages
